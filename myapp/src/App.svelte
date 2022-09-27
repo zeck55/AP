3030d5bcea24ceda92a9b5dfa64c8cb4d4bc5a1a
zeck55 <63237259+zeck55@users.noreply.github.com>
2022-09-27 14:17:58 +0200

    Navbar

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -2,9 +2,26 @@
 	import TestWebsite from './../../testWebsite.svelte';
   import Counter from './lib/Counter.svelte'
 
+export let menu = 1;
 </script>
 
 <main>
+
+  <style>
+	ul#menu li{
+		display : inline;
+	}
+</style>
+
+<ul id="menu">
+	<li><a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a></li> 
+	<li><a href="/" on:click|preventDefault={() => (menu = 2)}>Profile</a></li>
+</ul>
+
+{#if menu === 1}
+<TestWebsite />
+{/if}
+
 
 <div >
 <TestWebsite/>

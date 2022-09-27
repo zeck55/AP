76125700220477e0d470a0a8d1989f0f17e0f11f
zeck55 <63237259+zeck55@users.noreply.github.com>
2022-09-27 14:29:18 +0200

    Nav impor

--- testWebsite.svelte
+++ testWebsite.svelte
@@ -1,5 +1,21 @@
 
 
 <main>
-    <p>Hallo was geht </p>
+
+________________________________________________
+    
+    <p>Das wird die Home seit</p>
+
+        <div><!--Bild vom druck --><div>
+        <div><!--Bild vom druck --></div>
+        <div><!--Bild vom druck --></div>
+    <!--Navbar erstellen
+        Home 
+        kontaktfeld
+        Auftragsanahme
+        FAQ
+        impressum 
+        -->
+________________________________________________
+    <!---->
 </main>

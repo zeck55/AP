b955352baa0d9116bd897c516ca0e5cedf9e128e
zeck55 <63237259+zeck55@users.noreply.github.com>
2022-09-28 14:23:46 +0200

    fix

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,43 +1,45 @@
 <script>
-	import Kontaktfeld from './../../Kontaktfeld.svelte'
-	import TestWebsite from './../../testWebsite.svelte';
-
+  import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
-
-export let menu = 1;
 </script>
 
 <main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
 
-  <style>
-	ul#menu li{
-		display : inline;
-	}
-</style>
+  <div class="card">
+    <Counter />
+  </div>
 
-<ul id="menu">
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
 
-	<li><a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a></li> |
-	<li><a href="/" on:click|preventDefault={() => (menu = 2)}>Kontaktfeld</a></li> |
-  <li><a href ="/" on:click|preventDefault={() =>(menu = 4)}>Auftragsanahme</a></li>|
-  <li><a href ="/" on:click|preventDefault={() =>(menu = 3)}>FAQ</a></li>  |
-  <li><a href ="/" on:click|preventDefault={() =>(menu = 5)}>Impressum</a></li>
-  </ul>
-
-{#if menu === 1}
-<TestWebsite />
-{/if}
-{#if menu=== 2}
-<Kontaktfeld/>
-{/if}
-
-
-
-
-  
-  
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
 
 <style>
-
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>

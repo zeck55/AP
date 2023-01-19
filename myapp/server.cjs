c3b5b9a363550a0c66cea152f71f6599e1454c11
zeck55 <63237259+zeck55@users.noreply.github.com>
2023-01-19 15:05:11 +0100

    fix

--- myapp/server.cjs
+++ myapp/server.cjs
@@ -1,9 +1,26 @@
+import { update_await_block_branch } from 'svelte/internal';
+
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const port = process.env.PORT || 80;
 const path = require('path');
 
+const url
+ = "mongodb+srv://cluster0.0eoppxn.mongodb.net/myFirstDatabase"; --apiVersion1, --username, Admin;
 
+async function connect(){
+    try{
+        await mongoose.connect(url);
+        console.log("Connect to MongoDB");
+
+    }
+    catch (error){
+        console.error(error);
+    }
+}
+
+connect();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'index.html'))
--- myapp/src/main.js
+++ myapp/src/main.js
@@ -6,3 +6,18 @@ const app = new App({
 })
 
 export default app
+/*
+const mongodb = requier('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+const connectionString = 'mongodb://admin:admin@mongodb://localhost:27017/Kundendaten';
+
+MongoClient.connect(connectionString, {autoReconnect: true}, (err,dateabase) =>{
+  if (err) {
+    console.log('Failed to connect on the database .' , err,message);
+    process.exit(1);
+  }
+
+  console.log('Connected !');
+}); */
